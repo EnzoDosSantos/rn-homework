cdb0cecf834e925a75e2a3132d121d68f356affa
EnzoDosSantos <101943986+EnzoDosSantos@users.noreply.github.com>
2022-12-20 19:01:01 -0300

    ADD slint config

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/jsx-runtime'
+  ],
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'off': 'off',
+    'eslint-disable react/no-unescaped-entities': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    },
+  },
+};
