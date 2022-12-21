58fbe873a86a8ef472a52be2f294b281ede1fcfb
EnzoDosSantos <101943986+EnzoDosSantos@users.noreply.github.com>
2022-12-20 21:14:58 -0300

    ADD transactions screen

--- screens/Transactions.tsx
+++ screens/Transactions.tsx
@@ -1,14 +1,42 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
-import { Text, View } from '../components/Themed';
-import useUser from '../hooks/useGetterContext';
+import { Text, View } from "../components/Themed";
+import useUser from "../hooks/useGetterContext";
 
 export default function Transactions() {
-  const { transactions } = useUser()
+  const { transactions } = useUser();
+  console.log(transactions.transactions.length);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>transactions</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.title}>Transactions</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <Text style={styles.title}>Transactions history</Text>
+      {transactions.transactions.map((e, i) => {
+        return (
+          <View style={styles.transactionsContainer} key={i}>
+            {e.account && (
+              <Text style={styles.subTitle}>Account: {e.account}</Text>
+            )}
+            {e.toAccount && (
+              <Text style={styles.subTitle}>To the account: {e.toAccount}</Text>
+            )}
+            {e.toAccount && (
+              <Text style={styles.subTitle}>From account: {e.fromAccount}</Text>
+            )}
+            <Text style={styles.subTitle}>Amount: {e.amount}</Text>
+            <Text style={styles.subTitle}>Transfer type: {e.type}</Text>
+          </View>
+        );
+      })}
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
     </View>
   );
 }
@@ -16,16 +44,30 @@ export default function Transactions() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    marginTop: 20,
+    justifyContent: "center",
+  },
+  transactionsContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  subTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    margin: 5,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+  },
+  text: {
+    fontSize: 15,
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
